f2099441f024bfd04d0f104e74a586b82cd53a14
Cesar Lugo <cesarlugo00@hotmail.com>
2019-02-11 17:28:31 +0100

    shuffle function

--- deck.go
+++ deck.go
@@ -1,6 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
 
 type deck []string
 
@@ -24,5 +31,39 @@ func deal(d deck, handSize int) (deck, deck) {
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
+	}
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	s := strings.Split(string(bs), ",")
+	return deck(s)
+}
+
+func (d deck) shuffle() {
+	//using time package we can use current time to generate random int64 so we can start random number generation
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+	//see on documentation on math/rand package to see why this process to generate a random number
+
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+		d[i], d[newPosition] = d[newPosition], d[i]
+		// d[i] = d[newPosition]
+		// d[newPosition] = d[i]
 	}
 }
